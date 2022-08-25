30b03b741adbddbeb7b9d22de2a3433016c001db
Sleepingpirates <faizsyed.a@gmail.com>
2022-08-24 20:20:28 -0400

    Backend Api for creating user

---
+++ LAMPAPI/CreateUser.php
@@ -0,0 +1,44 @@
+
+<?php
+
+$inData = getRequestInfo();
+
+$id = 0;
+$firstName = $inData["firstName"];
+$lastName = $inData["LastName"];
+$Login =$inData["Login"];
+$Password=$inData["Password"];
+
+$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331"); 	
+if( $conn->connect_error )
+{
+    returnWithError( $conn->connect_error );
+}
+else
+{
+    $stmt = $conn->$conn->prepare("INSERT into Users (FirstName,LastName,Login,Password) VALUES(?,?,?,?)");
+    $stmt->bind_param("ss", $firstName, $lastName, $Login, $Password);
+    $stmt->execute();
+    $stmt->close();
+    $conn->close();
+    returnWithError("");
+}
+
+function getRequestInfo()
+{
+    return json_decode(file_get_contents('php://input'), true);
+}
+
+function sendResultInfoAsJson( $obj )
+{
+    header('Content-type: application/json');
+    echo $obj;
+}
+
+function returnWithError( $err )
+{
+    $retValue = '{"error":"' . $err . '"}';
+    sendResultInfoAsJson( $retValue );
+}
+
+?>
