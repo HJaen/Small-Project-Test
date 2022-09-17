dab46db359fa6ed2d9028adffb9f84b58726117a
HJaen <hjaenscode@gmail.com>
2022-09-17 09:32:52 -0400

    Editted search to find string in the middle and end of names too

--- LAMPAPI/SearchContacts.php
+++ LAMPAPI/SearchContacts.php
@@ -3,17 +3,14 @@
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
     $inData = getRequestInfo();
-	$search = $inData["search"] . "%";
+	$search = "%" . $inData["search"] . "%";
 	$ID = $inData["ID"];
 	$pageNumber = $inData["PageNumber"];
 	$no_of_records_per_page = 35;
 	$offset = ($pageNumber-1) * $no_of_records_per_page;
 	$foundResults = 0;
 
-    //change later with the server's credentials
-	//mysqli(server host name, username to log into database, database password, said database)
 	$conn = new mysqli("localhost", "TheBeast", "WeLoveCOP4331", "COP4331");
-	//$conn = new mysqli("localhost", "root", "0afdbc3e76a812133be14d1c737c6766a3988364f36eb65d", "Contacts");
     if ($conn->connect_error) 
 	{
 		returnWithError( $conn->connect_error );
@@ -27,10 +24,6 @@
 		$TotalCount = $data[0];
 
 		$query = "SELECT * FROM Contacts WHERE UserID=$ID AND (FirstName like '$desired' or LastName like '$desired') LIMIT $offset, $no_of_records_per_page";
-		
-		//$stmt->bind_param("sssss", $desired, $desired, $desired, $desired, $inData["userID"]);	Should the userID be returned as well? Or just the F and L name, phone and email?
-		//$stmt->bind_param("ssss", $desired, $desired, $desired, $desired);
-		//$stmt->execute();
 
 		$stmt = $conn->query($query);
 		
